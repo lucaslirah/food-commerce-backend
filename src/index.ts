263dcc896f144b96136afaea83fd99dc62961008
Lucas Lira <lucasliradev@gmail.com>
2024-10-31 15:34:50 -0300

    configuração inicial com express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello world!")
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello, world!");
+});
+
+app.listen(port, () => console.log('Listening on port ' + port));
